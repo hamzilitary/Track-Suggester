31c4c4be91e9049f25b2db51cede36c5d3844310
Hamza Naeem <hamzilitary22@gmail.com>
2018-01-12 14:04:32 -0800

    change the directory name due to syntax, add javascript commands, and add 'div id' to appear after submit button is clicked

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    var end = $("select#end").val();
+    var path = $("select#path").val();
+
+    if (end === 'front' && path === 'webd') {
+      $('#css').show();
+    }
+    if (end === 'front' && path === 'mobile') {
+      $('#css').show();
+    }
+    if (end === 'back' && path === 'webd') {
+      $('#csharp').show();
+    }
+    if (end === 'back' && path === 'mobile') {
+      $('')
+    }
+  event.preventDefault();
+});
+});
